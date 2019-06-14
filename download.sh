1c7df4652f1bf59e1d39c08f618cf5ffff7154fd
Julian Straub <Julian.Straub@oculus.com>
2019-06-14 08:34:37 -0700

    update the download script to reflect the version of the release

--- download.sh
+++ download.sh
@@ -9,7 +9,7 @@ fi
 
 for p in {a..q}
 do 
-  wget https://github.com/facebookresearch/Replica-Dataset/releases/download/v1/replica_v1_0.tar.gz.parta$p
+  wget https://github.com/facebookresearch/Replica-Dataset/releases/download/v1.0/replica_v1_0.tar.gz.parta$p
 done
 
 cat replica_v1_0.tar.gz.part?? | unpigz -p 32  | tar -xvC $1
